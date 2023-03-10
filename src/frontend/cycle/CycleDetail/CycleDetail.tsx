f4bc565e0b226bd01501c93063ee2715926dce6c
jairajdev <jairajranamagar.dev@gmail.com>
2023-03-10 17:34:55 +0900

    Update the cycleRecord page

--- src/frontend/cycle/CycleDetail/CycleDetail.tsx
+++ src/frontend/cycle/CycleDetail/CycleDetail.tsx
@@ -11,7 +11,7 @@ import { breadcrumbsList } from "../../types";
 export const CycleDetail: React.FC = () => {
   const router = useRouter();
 
-  const id = router?.query?.id ? router.query.id : '0' as String;
+  const id = router?.query?.id ? router.query.id : ("0" as String);
 
   const { data } = useCycleDetail(id);
 
@@ -37,114 +37,144 @@ export const CycleDetail: React.FC = () => {
         breadcrumbItems={breadcrumbs}
         showBackButton
       >
-        <div className={styles.card}>
-          <div className={styles.item}>
-            <div className={styles.title}>Network ID</div>
-            <div className={styles.value}>{data?.cycleRecord?.networkId}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Network State Hash</div>
-            <div className={styles.value}>
-              {data?.cycleRecord?.networkStateHash || "-"}
+        {data ? (
+          <div className={styles.card}>
+            <div className={styles.item}>
+              <div className={styles.title}>Network ID</div>
+              <div className={styles.value}>{data?.cycleRecord?.networkId}</div>
             </div>
-          </div>
 
-          <div className={styles.item}>
-            <div className={styles.title}>Cycle Number</div>
-            <div className={styles.value}>{data?.cycleRecord?.counter}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Cycle Marker</div>
-            <div className={styles.value}>{data?.cycleRecord?.marker}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Cycle Start Time</div>
-            <div className={styles.value}>
-              {new Date(data?.cycleRecord?.start * 1000).toString()}
+            <div className={styles.item}>
+              <div className={styles.title}>Network State Hash</div>
+              <div className={styles.value}>
+                {data?.cycleRecord?.networkStateHash || "-"}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.item}>
-            <div className={styles.title}>Cycle Duration (seconds)</div>
-            <div className={styles.value}>{data?.cycleRecord?.duration}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Number of Active Nodes</div>
-            <div className={styles.value}>{data?.cycleRecord?.active}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Number of Syncing Nodes</div>
-            <div className={styles.value}>{data?.cycleRecord?.syncing}</div>
-          </div>
-
-          <div className={styles.item}>
-            <div className={styles.title}>Expired Nodes</div>
-            <div className={styles.value}>{data?.cycleRecord?.expired}</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Removed Nodes</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Returned Nodes</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Lost Nodes</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Joined Nodes</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Activated Nodes</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Activated Public Keys</div>
-            <div className={styles.value}>-</div>
-          </div>
-
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Refreshed Archivers</div>
-            <div className={styles.value}>
-              758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3
+            <div className={styles.item}>
+              <div className={styles.title}>Cycle Number</div>
+              <div className={styles.value}>{data?.cycleRecord?.counter}</div>
             </div>
-          </div>
 
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Refreshed Concensors</div>
-            <div className={styles.value}>
-              de366f3148e3bc06096f7ed8952d213cb09ec47e34d78b7c9bbbcfd241948c39
+            <div className={styles.item}>
+              <div className={styles.title}>Cycle Marker</div>
+              <div className={styles.value}>{data?.cycleRecord?.marker}</div>
             </div>
-          </div>
 
-          {/* TODO: calculate removed nodes data?.cycleRecord?.removed */}
-          <div className={styles.item}>
-            <div className={styles.title}>Partition Blocks</div>
-            <div className={styles.value}>-</div>
+            <div className={styles.item}>
+              <div className={styles.title}>Cycle Start Time</div>
+              <div className={styles.value}>
+                {new Date(data?.cycleRecord?.start * 1000).toString()}
+              </div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Cycle Duration (seconds)</div>
+              <div className={styles.value}>{data?.cycleRecord?.duration}</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Number of Active Nodes</div>
+              <div className={styles.value}>{data?.cycleRecord?.active}</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Number of Syncing Nodes</div>
+              <div className={styles.value}>{data?.cycleRecord?.syncing}</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Expired Nodes</div>
+              <div className={styles.value}>{data?.cycleRecord?.expired}</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Removed Nodes</div>
+              <div className={styles.value}> { data?.cycleRecord?.removed?.length > 0  ?
+                data?.cycleRecord?.removed?.map((item) => {
+                  return <div>{item}</div>;
+                }):
+                '-'
+              }</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Lost Nodes</div>
+              <div className={styles.value}>{
+                data?.cycleRecord?.lost?.length > 0  ?
+                data?.cycleRecord?.lost?.map((item) => {
+                  return <div>{item}</div>;
+                }): '-'
+              }</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Returned Nodes</div>
+              <div className={styles.value}>{
+                data?.cycleRecord?.refuted?.length > 0  ?
+                data?.cycleRecord?.refuted?.map((item) => {
+                  return <div>{item}</div>;
+                }) : '-'
+              }</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Joined Nodes</div>
+              <div className={styles.value}>{
+                data?.cycleRecord?.joinedConsensors?.length > 0  ?
+                data?.cycleRecord?.joinedConsensors?.map((item) => {
+                  return <div>{item.id}</div>;
+                }) : '-'
+              }</div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Activated Nodes</div>
+              <div className={styles.value}>{
+                data?.cycleRecord?.actived?.length > 0  ?
+                data?.cycleRecord?.actived?.map((item) => {
+                  return <div>{item.id}</div>;
+                }) : '-'
+              }</div>
+            </div>
+
+            {/* <div className={styles.item}>
+              <div className={styles.title}>Activated Public Keys</div>
+              <div className={styles.value}>-</div>
+            </div> */}
+
+            <div className={styles.item}>
+              <div className={styles.title}>Refreshed Archivers</div>
+              <div className={styles.value}>
+              {
+                data?.cycleRecord?.refreshedArchivers?.length > 0  ?
+                data?.cycleRecord?.refreshedArchivers?.map((item) => {
+                  return <div>{item.publicKey}</div>;
+                }) : '-'
+              }
+              </div>
+            </div>
+
+            <div className={styles.item}>
+              <div className={styles.title}>Refreshed Concensors</div>
+              <div className={styles.value}>
+              {
+                data?.cycleRecord?.refreshedConsensors?.length > 0  ?
+                data?.cycleRecord?.refreshedConsensors?.map((item) => {
+                  return <div>{item.id}</div>;
+                }) : '-'
+              }
+              </div>
+            </div>
+
+            {/* <div className={styles.item}>
+              <div className={styles.title}>Partition Blocks</div>
+              <div className={styles.value}>-</div>
+            </div> */}
           </div>
-        </div>
+        ) : (
+          <div> No Data</div>
+        )}
       </ContentLayout>
     </div>
   );
