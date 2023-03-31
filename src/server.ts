17e7564e03f351e89861c3cdf9e8be530a091de3
Arham Jain <arham.nec@gmail.com>
2023-03-31 15:13:10 +0530

    fix(list): tx stats

--- src/server.ts
+++ src/server.ts
@@ -1236,16 +1236,15 @@ const start = async () => {
     }
     if (query.responseType && query.responseType === 'array') {
       let temp_array = []
-      if (temp_array.length > 0)
-        transactionStats.forEach((item) =>
-          temp_array.push([
-            item.timestamp * 1000,
-            item.totalTxs,
-            item.totalStakeTxs,
-            item.totalUnstakeTxs,
-            item.cycle,
-          ])
-        )
+      transactionStats.forEach((item) =>
+        temp_array.push([
+          item.timestamp * 1000,
+          item.totalTxs,
+          item.totalStakeTxs,
+          item.totalUnstakeTxs,
+          item.cycle,
+        ])
+      )
       transactionStats = temp_array
     }
     const res = {
