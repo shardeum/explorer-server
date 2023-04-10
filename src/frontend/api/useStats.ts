504789e53b3e0fdda88a97f5657e4e50547b9674
municorn <municorn@musicaloft.com>
2023-04-10 17:18:17 -0500

    frontend/useStats: Overhaul to accommodate for useSWR better
    also add comments and better organization of code

--- src/frontend/api/useStats.ts
+++ src/frontend/api/useStats.ts
@@ -1,45 +1,44 @@
-import useSWR, { SWRResponse } from 'swr'
+import useSWR from 'swr'
 
 import { fetcher } from './fetcher'
 
 import { PATHS } from './paths'
 
-export const useStats = (query: any) => {
+export const useStats = (query: {
+  validatorStatsCount?: number
+  transactionStatsCount?: number
+  fetchCoinStats?: boolean
+}) => {
   const { validatorStatsCount, transactionStatsCount, fetchCoinStats } = query
 
-  // console.log(fetcher, `${PATHS.STATS_VALIDATOR}?count=${count}&responseType=array`);
-  const loading = true
+  // set query paths to `null` if we shouldn't fetch them
+  const validatorStatsQuery = validatorStatsCount
+    ? `${PATHS.STATS_VALIDATOR}?count=${validatorStatsCount}&responseType=array`
+    : null
+  const transactionStatsQuery = transactionStatsCount
+    ? `${PATHS.STATS_TRANSACTION}?count=${transactionStatsCount}&responseType=array`
+    : null
+  const coinStatsQuery = fetchCoinStats ? `${PATHS.STATS_COIN}` : null
 
-  let response: SWRResponse<any, any>
-  let validatorStats: any[] = []
-  let transactionStats: any[] = []
-  let totalSHM = 0
-  let totalStakedSHM = 0
+  // get responses
+  const validatorStatsResponse = useSWR(validatorStatsQuery, fetcher)
+  const transactionStatsResponse = useSWR(transactionStatsQuery, fetcher)
+  const coinStatsResponse = useSWR(coinStatsQuery, fetcher)
 
-  if (validatorStatsCount) {
-    response = useSWR(`${PATHS.STATS_VALIDATOR}?count=${validatorStatsCount}&responseType=array`, fetcher)
-    // console.log("data", data);
-
-    validatorStats = response?.data?.validatorStats || []
-  }
-
-  if (transactionStatsCount) {
-    response = useSWR(`${PATHS.STATS_TRANSACTION}?count=${transactionStatsCount}&responseType=array`, fetcher)
-
-    transactionStats = response?.data?.transactionStats || []
-  }
-
-  if (fetchCoinStats) {
-    response = useSWR(`${PATHS.STATS_COIN}`, fetcher)
-    totalSHM = response?.data?.totalSupply || 0
-    totalStakedSHM = response?.data?.totalStaked || 0
-  }
+  // get values
+  const validatorStats = validatorStatsResponse?.data?.validatorStats || []
+  const transactionStats = transactionStatsResponse?.data?.transactionStats || []
+  const totalSHM = coinStatsResponse?.data?.totalSupply || 0
+  const totalStakedSHM = coinStatsResponse?.data?.totalStaked || 0
 
   return {
     validatorStats,
     transactionStats,
     totalSHM,
     totalStakedSHM,
-    loading: !loading,
+    loading:
+      validatorStatsResponse?.isValidating ||
+      transactionStatsResponse?.isValidating ||
+      coinStatsResponse?.isValidating,
   }
 }
