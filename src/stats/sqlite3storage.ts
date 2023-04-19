34e3abc2ceb15ac0466e87c23391d32fccd66b90
municorn <municorn@musicaloft.com>
2023-04-18 21:24:16 -0500

    stats/sqlite3storage: Fix import lint issue

--- src/stats/sqlite3storage.ts
+++ src/stats/sqlite3storage.ts
@@ -4,10 +4,11 @@ import { ValidatorStats } from "./validatorStats"
 
 /***
 This is the copied code from 'storage/sqlite3storage.ts'
-Later, extract the commom functions into one place and refactor it to be able to use them in both places.
+Later, extract the common functions into one place and refactor it to be able to use them in both places.
 ***/
-const sqlite3 = require('sqlite3').verbose()
-let db: any
+import sqlite3Lib from 'sqlite3'
+const sqlite3 = sqlite3Lib.verbose()
+let db: sqlite3Lib.Database
 
 export async function init() {
   db = new sqlite3.Database('statsDB.sqlite3')
