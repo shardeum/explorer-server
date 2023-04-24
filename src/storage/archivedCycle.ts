d0871f7d933afc34a4cc859297b9797af0fef169
municorn <municorn@musicaloft.com>
2023-04-24 13:47:29 -0500

    storage/archivedCycle: Make `ArchivedCycle.counter` optional

--- src/storage/archivedCycle.ts
+++ src/storage/archivedCycle.ts
@@ -6,7 +6,7 @@ import { StateManager, P2P } from '@shardus/types'
 export let Collection: unknown
 
 export interface ArchivedCycle {
-  counter: number
+  counter?: number
   cycleRecord: P2P.CycleCreatorTypes.CycleRecord
   cycleMarker: StateManager.StateMetaDataTypes.CycleMarker
   data: StateManager.StateMetaDataTypes.StateData
