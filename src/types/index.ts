474fd0080fcce0db83408dde6688aeed6d96e288
municorn <municorn@musicaloft.com>
2023-06-05 11:41:08 -0500

    types: Move transaction types into their own file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,66 +1,9 @@
-import { Token } from '../storage/account'
-import { WrappedEVMAccount } from './account'
-
-export enum TransactionType {
-  Receipt = 0, // EVM Receipt
-  NodeRewardReceipt = 1,
-  StakeReceipt = 2,
-  UnstakeReceipt = 3,
-  EVM_Internal = 4,
-  ERC_20 = 5,
-  ERC_721 = 6,
-  ERC_1155 = 7,
-  InternalTxReceipt = 8,
-}
-
 export interface ERC20ContractDetail {
   name: string
   decimals: string
   symbol: string
   totalSupply: string
   txHash: string
-}
-
-export interface TokenTx<C = string> {
-  cycle: number
-  timestamp: number
-  contractAddress: string
-  contractInfo?: C
-  tokenFrom: string
-  tokenTo: string
-  tokenValue: string
-  tokenType: TransactionType
-  tokenEvent: string
-  tokenOperator?: string | null
-  transactionFee: string
-
-  // references another tx
-  txId?: string
-  txHash: string
-}
-
-export enum InternalTXType {
-  SetGlobalCodeBytes,
-  InitNetwork,
-  NodeReward,
-}
-
-/**
- * InternalTx is a non EVM TX that shardeum can use for utility task such as global changes
- *
- */
-export interface InternalTxBase {
-  isInternalTx: boolean
-  internalTXType: InternalTXType
-}
-
-export interface InternalTx extends InternalTxBase {
-  timestamp: number
-  from?: string
-  to?: string
-  accountData?: WrappedEVMAccount
-  network?: string // Network Account
-  nodeId?: string // Node Account
 }
 
 export interface NetworkParameters {
@@ -74,27 +17,7 @@ export interface NetworkParameters {
   maintenanceFee: number
 }
 
-export interface DecodeTxResult {
-  txs: TokenTx[]
-  accs: string[] // ethAddress[]
-  tokens: Token[]
-}
-
-export enum TransactionSearchType {
-  All = 0, // Receipt + NodeRewardReceipt + StakeReceipt + UnstakeReceipt + InternalTxReceipt
-  Receipt = 1,
-  NodeRewardReceipt = 2,
-  StakeReceipt = 3,
-  UnstakeReceipt = 4,
-  EVM_Internal = 5,
-  ERC_20 = 6,
-  ERC_721 = 7,
-  ERC_1155 = 8,
-  TokenTransfer = 9, // token txs of a contract
-  InternalTxReceipt = 10,
-  AllExceptInternalTx = 11, // Receipt + NodeRewardReceipt + StakeReceipt + UnstakeReceipt (exclude InternalTxReceipt)
-}
-
 export * from './account'
 export * from './receipt'
 export * from './serverResponseTypes'
+export * from './transaction'
