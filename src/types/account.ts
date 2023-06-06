234eb8554a61dbababc98d0a04e33fc513f780f1
municorn <municorn@musicaloft.com>
2023-06-06 10:37:38 -0500

    types: Move account types into their own file

---
+++ src/types/account.ts
@@ -0,0 +1,148 @@
+import { Account } from 'ethereumjs-util'
+import { TxReceipt } from '@ethereumjs/vm/dist/types'
+import { ReadableReceipt } from './receipt'
+import { ERC20ContractDetail } from '.'
+import { TokenTx } from './transaction'
+
+export enum AccountType {
+  Account, //  EOA or CA
+  ContractStorage, // Contract storage key value pair
+  ContractCode, // Contract code bytes
+  Receipt, //This holds logs for a TX
+  Debug,
+  NetworkAccount,
+  NodeAccount,
+  NodeRewardReceipt,
+  DevAccount,
+  NodeAccount2,
+  StakeReceipt,
+  UnstakeReceipt,
+  InternalTxReceipt,
+}
+
+export enum AccountSearchType {
+  All, // All Accounts Type
+  EOA,
+  CA,
+  GENERIC, // Generic Contract Type
+  ERC_20,
+  ERC_721,
+  ERC_1155,
+  Receipt, // EVM Receipt
+  StakeReceipt,
+  UnstakeReceipt,
+  NetworkAccount,
+  NodeAccount,
+  NodeRewardReceipt,
+  NodeAccount2,
+  ContractStorage,
+  ContractCode,
+  InternalTxReceipt,
+}
+
+export interface BaseAccount {
+  accountType: AccountType
+}
+
+interface BaseWrappedEVMAccount extends BaseAccount {
+  /** account address in EVM space. can have different meanings depending on account type */
+  ethAddress: string
+
+  /** account hash */
+  hash: string
+
+  /** account timestamp. last time a TX changed it */
+  timestamp: number
+}
+
+/**
+ * Still working out the details here.
+ * This has become a variant data type now that can hold an EVM account or a key value pair from CA storage
+ * I think that is the shortest path for now to get syncing and repair functionality working
+ *
+ * Long term I am not certain if we will be able to hold these in memory.  They may have to be a temporary thing
+ * that is held in memory for awhile but eventually cleared.  This would mean that we have to be able to pull these
+ * from disk again, and that could be a bit tricky.
+ */
+export type WrappedEVMAccount = BaseWrappedEVMAccount &
+  (WrappedDataReceipt | WrappedDataAccount | WrappedDataContractStorage | WrappedDataContractCode)
+
+/** Variant data: account */
+export interface WrappedDataAccount {
+  accountType: AccountType.Account
+  account: Account
+}
+
+/** Variant data: contract storage */
+export interface WrappedDataContractStorage {
+  accountType: AccountType.ContractStorage
+
+  /** EVM CA storage key */
+  key: string
+
+  /** EVM buffer value if this is of type CA_KVP */
+  value: Buffer
+}
+
+/** Variant data: contract code related and addresses */
+export interface WrappedDataContractCode {
+  accountType: AccountType.ContractCode
+
+  codeByte: Buffer
+  codeHash: { data: Buffer }
+  contractAddress: string
+}
+
+/** Variant data: receipt related */
+export interface WrappedDataReceipt {
+  accountType:
+    | AccountType.Receipt
+    | AccountType.NodeRewardReceipt
+    | AccountType.StakeReceipt
+    | AccountType.UnstakeReceipt
+    | AccountType.InternalTxReceipt
+
+  /** For debug tx */
+  balance: string
+  amountSpent: string
+  contractInfo: ERC20ContractDetail
+  nonce: string
+  readableReceipt: ReadableReceipt
+  receipt: TxReceipt
+  tokenTx: TokenTx
+  txFrom: string
+  txId: string
+}
+
+export interface NodeAccount extends BaseAccount {
+  id: string
+  balance: number
+  nodeRewardTime: number
+  hash: string
+  timestamp: number
+}
+
+/** Unrelated to `WrappedEVMAccount`,  */
+export interface WrappedAccount {
+  accountId: string
+  stateId: string
+  data: any
+  timestamp: number
+  accountCreated?: boolean
+}
+
+export interface NetworkAccount extends BaseAccount {
+  id: string
+  current: NetworkParameters
+  next: NetworkParameters | {}
+  hash: string
+  timestamp: number
+}
+
+export interface NodeAccount extends BaseAccount {
+  id: string
+  balance: number
+  nodeRewardTime: number
+  hash: string
+  timestamp: number
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,5 @@
-import { Account } from 'ethereumjs-util'
-import { TxReceipt } from '@ethereumjs/vm/dist/types'
 import { Token } from '../storage/account'
-
-export enum AccountType {
-  Account, //  EOA or CA
-  ContractStorage, // Contract storage key value pair
-  ContractCode, // Contract code bytes
-  Receipt, //This holds logs for a TX
-  Debug,
-  NetworkAccount,
-  NodeAccount,
-  NodeRewardReceipt,
-  DevAccount,
-  NodeAccount2,
-  StakeReceipt,
-  UnstakeReceipt,
-  InternalTxReceipt,
-}
+import { WrappedEVMAccount } from './account'
 
 export enum TransactionType {
   Receipt = 0, // EVM Receipt
@@ -28,84 +11,6 @@ export enum TransactionType {
   ERC_721 = 6,
   ERC_1155 = 7,
   InternalTxReceipt = 8,
-}
-
-export interface BaseAccount {
-  accountType: AccountType
-}
-
-interface BaseWrappedEVMAccount extends BaseAccount {
-  /** account address in EVM space. can have different meanings depending on account type */
-  ethAddress: string
-
-  /** account hash */
-  hash: string
-
-  /** account timestamp. last time a TX changed it */
-  timestamp: number
-}
-
-/**
- * Still working out the details here.
- * This has become a variant data type now that can hold an EVM account or a key value pair from CA storage
- * I think that is the shortest path for now to get syncing and repair functionality working
- *
- * Long term I am not certain if we will be able to hold these in memory.  They may have to be a temporary thing
- * that is held in memory for awhile but eventually cleared.  This would mean that we have to be able to pull these
- * from disk again, and that could be a bit tricky.
- */
-export type WrappedEVMAccount = BaseWrappedEVMAccount & (
-    | WrappedDataReceipt
-    | WrappedDataAccount
-    | WrappedDataContractStorage
-    | WrappedDataContractCode
-  )
-
-/** Variant data: account */
-export interface WrappedDataAccount {
-  accountType: AccountType.Account
-  account: Account
-}
-
-/** Variant data: contract storage */
-export interface WrappedDataContractStorage {
-  accountType: AccountType.ContractStorage
-
-  /** EVM CA storage key */
-  key: string
-
-  /** EVM buffer value if this is of type CA_KVP */
-  value: Buffer
-}
-
-/** Variant data: contract code related and addresses */
-export interface WrappedDataContractCode {
-  accountType: AccountType.ContractCode
-
-  codeByte: Buffer
-  codeHash: { data: Buffer }
-  contractAddress: string
-}
-
-/** Variant data: receipt related */
-export interface WrappedDataReceipt {
-  accountType:
-    | AccountType.Receipt
-    | AccountType.NodeRewardReceipt
-    | AccountType.StakeReceipt
-    | AccountType.UnstakeReceipt
-    | AccountType.InternalTxReceipt
-
-  /** For debug tx */
-  balance: string
-  amountSpent: string
-  contractInfo: ERC20ContractDetail
-  nonce: string
-  readableReceipt: ReadableReceipt
-  receipt: TxReceipt
-  tokenTx: TokenTx
-  txFrom: string
-  txId: string
 }
 
 export interface ReadableReceipt {
@@ -185,22 +90,6 @@ export interface InternalTx extends InternalTxBase {
   nodeId?: string // Node Account
 }
 
-export interface WrappedAccount {
-  accountId: string
-  stateId: string
-  data: any
-  timestamp: number
-  accountCreated?: boolean
-}
-
-export interface NetworkAccount extends BaseAccount {
-  id: string
-  current: NetworkParameters
-  next: NetworkParameters | {}
-  hash: string
-  timestamp: number
-}
-
 export interface NetworkParameters {
   title: string
   description: string
@@ -212,38 +101,10 @@ export interface NetworkParameters {
   maintenanceFee: number
 }
 
-export interface NodeAccount extends BaseAccount {
-  id: string
-  balance: number
-  nodeRewardTime: number
-  hash: string
-  timestamp: number
-}
-
 export interface DecodeTxResult {
   txs: TokenTx[]
   accs: string[] // ethAddress[]
   tokens: Token[]
-}
-
-export enum AccountSearchType {
-  All, // All Accounts Type
-  EOA,
-  CA,
-  GENERIC, // Generic Contract Type
-  ERC_20,
-  ERC_721,
-  ERC_1155,
-  Receipt, // EVM Receipt
-  StakeReceipt,
-  UnstakeReceipt,
-  NetworkAccount,
-  NodeAccount,
-  NodeRewardReceipt,
-  NodeAccount2,
-  ContractStorage,
-  ContractCode,
-  InternalTxReceipt,
 }
 
 export enum TransactionSearchType {
@@ -261,4 +122,5 @@ export enum TransactionSearchType {
   AllExceptInternalTx = 11, // Receipt + NodeRewardReceipt + StakeReceipt + UnstakeReceipt (exclude InternalTxReceipt)
 }
 
+export * from './account'
 export * from './serverResponseTypes'
