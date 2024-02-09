7270902127776c7fee01d6814b9b46c2d8ff0d36
Sam Sweet <sam.w.sweet@gmail.com>
2024-02-09 12:33:20 -0500

    add return type to debounced function

--- src/frontend/utils/debounce.ts
+++ src/frontend/utils/debounce.ts
@@ -1,6 +1,6 @@
 export function debounce(func, wait): (...args: any[]) => void {
   let timeout
-  return function executedFunction(...args) {
+  return function executedFunction(...args): void {
     const later = (): void => {
       clearTimeout(timeout)
       func(...args)
